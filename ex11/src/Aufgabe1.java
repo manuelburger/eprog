461f6ec7c06d48b8bbf05c9a0e0bb1c186696bbc
Manuel Burger <burgerm@student.ethz.ch>
2020-12-03 11:49:47 +0100

    Update Aufgabe1.java
    Remove solution for Aufgabe1

--- ex11/src/Aufgabe1.java
+++ ex11/src/Aufgabe1.java
@@ -7,32 +7,7 @@ public class Aufgabe1 {
 
 		ArrayList<String> allWords = new ArrayList<String>();
 		Scanner input = new Scanner(new File("aufgabentext1.txt"));
-		while (input.hasNext()) {
-			String word = input.next();
-			System.out.println(".." + word);
-			allWords.add(word);
-		}
-		input.close();
 
-		System.out.println(allWords);
-
-		for (int i = allWords.size() - 1; i >= 0; i--) {
-			System.out.println(allWords.get(i));
-		}
-
-		for (int i = 0; i < allWords.size(); i++) {
-			String s = allWords.get(i);
-			if (Character.isUpperCase(s.charAt(0)))
-				System.out.println(s.toUpperCase());
-		}
-
-		for (int i = 0; i < allWords.size(); i++) {
-			String s = allWords.get(i);
-			if (Character.isUpperCase(s.charAt(0))) {
-				allWords.remove(i);
-				i--;
-			}
-		}
 
 		System.out.println(allWords);
 
