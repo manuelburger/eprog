ed533adff45f5213067eb09257dd3bff8a9ad1cb
Manuel Burger <burgerm@student.ethz.ch>
2020-12-03 14:00:10 +0100

    Add HS17 Aufgabe 1

---
+++ exams/hs17/Aufgabe1.java
@@ -0,0 +1,27 @@
+import java.util.Set;
+import java.util.HashSet;
+
+class Aufgabe1 {
+
+    public static void main(String[] args) {
+
+        String t = "ab";
+        String s = "12";
+
+        Set<String> solution = verzahnung(s, t);
+
+        System.out.print("Solution: ");
+        System.out.println(solution);
+
+    }
+
+
+    public static Set<String> verzahnung(String s, String t) {
+        Set<String> solution = new HashSet<String>();
+            
+        return solution;
+    }
+
+}
+
+
